f3db5540f7674d1a80d7c4742471e4b0bd7050b2
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2024-09-19 23:38:38 +0900

    ブチブチするけど落ちない

--- src/lib.rs
+++ src/lib.rs
@@ -130,7 +130,10 @@ impl Plugin for RubatoDownsampler {
         nih_log!("Initializing plugin");
         self.sample_rate = buffer_config.sample_rate;
         let resample_rate = self.params.resample.value();
-        self.resample_ratio = resample_rate as f64 / self.sample_rate as f64;
+        nih_log!("Resample rate: {} Hz", resample_rate);
+        self.resample_ratio = self.sample_rate as f64 / resample_rate as f64;
+        // self.resample_ratio = resample_rate as f64 / self.sample_rate as f64;
+        nih_log!("Resample ratio: {}", self.resample_ratio);
         self.resampler_in
             .set_resample_ratio(self.resample_ratio, false)
             .expect("Failed to set resample ratio to resampler_in");
@@ -159,6 +162,7 @@ impl Plugin for RubatoDownsampler {
             nih_log!("Resample rate changed: {} Hz", resample_rate);
 
             self.resample_ratio = resample_ratio;
+            nih_log!("New resample ratio: {}", self.resample_ratio);
             //TODO 関数化
             self.resampler_in
                 .set_resample_ratio(resample_ratio, false)
